72497e2f852a73e6f7dca1a0d12d3cf0091ae7c2
Vipin <vipin@localhost.localdomain>
2020-12-24 23:52:04 +0530

    added some fetch endpoints with api key

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY ="dd43214987587e02d6e7a38b0761b3f2";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_geners=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_geners=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_geners=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_geners=99`,
+}
+
+export default requests;
